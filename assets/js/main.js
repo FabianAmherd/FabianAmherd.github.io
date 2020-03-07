baa020c6624e244b2ac7b71c3760771b86e7ef3c
Elias Rodriguez <elias.rodriguez.privat@gmail.com>
2020-03-07 18:30:48 +0100

    hero animation test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('nav-links li');
-const sunPath = "M51 25.5C51 39.5833 39.5833 51 25.5 51C11.4167 51 0 39.5833 0 25.5C0 11.4167 11.4167 0 25.5 0C39.5833 0 51 11.4167 51 25.5Z";
-const moonPath = "M12.5 25.5C12.5 36.2017 20.0065 45.3636 23.6114 49.1464C24.2478 49.8142 23.7677 50.9587 22.8501 50.864C10.012 49.5384 0 38.6884 0 25.5C0 12.3116 10.012 1.46157 22.8501 0.136028C23.7677 0.0412854 24.2478 1.18576 23.6114 1.85356C20.0065 5.63639 12.5 14.7983 12.5 25.5Z";
 
 const heroButton = document.querySelector('.hero-button')
 
@@ -17,14 +15,12 @@ heroButton.addEventListener('click', () => {
    })
 
    .add({
-      targets: '.hero-title>span>span',
-      opacity: 1,
+      targets: '.word1',
       delay: 1800,
-      duration: 1500,
+      duration: 2000,
       translateY: ['-100%', '0%'],
       easing: 'easeOutExpo'
    })
-   
 
    .add({
       targets: 'nav',
@@ -34,6 +30,31 @@ heroButton.addEventListener('click', () => {
       easing: 'easeOutExpo',
       opacity: 1,
    })
+
+   anime({
+      targets: '.word2',
+      delay: 2900,
+      duration: 2000,
+      translateY: ['-100%', '0%'],
+      easing: 'easeOutExpo'
+   })
+
+   anime({
+      targets: '.word3',
+      delay: 3000,
+      duration: 2000,
+      translateY: ['-100%', '0%'],
+      easing: 'easeOutExpo'
+   })
+
+   anime({
+      targets: '.word4',
+      delay: 3100,
+      duration: 2000,
+      translateY: ['-100%', '0%'],
+      easing: 'easeOutExpo'
+   })
+
 
    
 });
