1f5dca1dbd9b348ea3f62f1c5e04af32ea1118c1
Fabian Amherd <fabian.amherd@gmail.com>
2020-06-19 19:17:52 +0200

    Tried to add swup to working contact form

--- fullscreenform.php
+++ fullscreenform.php
@@ -37,7 +37,7 @@ if(filter_has_var(INPUT_POST, 'submit')) {
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/form.css" />
-		<title>Kontakt | Latify</title>
+		<title>Kontaktformular | Latify</title>
 		<script src="assets/js/modernizr.custom.js"></script>
 		<!-- Favicons -->
 		<link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
@@ -51,45 +51,46 @@ if(filter_has_var(INPUT_POST, 'submit')) {
 		<meta name="theme-color" content="#ffffff">
 	</head>
 	<body>
-
-		<div class="container">
-			<div class="fs-form-wrap" id="fs-form-wrap">
-
-
-				
-				<form id="myform" class="fs-form fs-form-full" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" target="_blank" autocomplete="off">
-					<ol class="fs-fields">
-						<li>
-							<label class="fs-field-label fs-anim-upper" for="q1">Wie lautet Ihr Name?</label>
-							<input class="fs-anim-lower" id="q1" name="q1" type="text" placeholder="Ihr Name" spellcheck="false" value="<?php echo isset($_POST['q1']) ? $name : ''; ?>" required/>
-						</li>
-						<li>
-							<label class="fs-field-label fs-anim-upper" for="q2">Für wen arbeiten Sie?</label>
-							<input class="fs-anim-lower" id="q2" name="q2" type="text" placeholder="Ihre Firma / Selbstständig" spellcheck="false" value="<?php echo isset($_POST['q2']) ? $employer : ''; ?>" required/>
-						</li>
-						<li>
-							<label class="fs-field-label fs-anim-upper" for="q3">Wo dürfen wir Ihnen antworten?</label>
-							<input class="fs-anim-lower" id="q3" name="q3" type="email" placeholder="hello@latify.ch" spellcheck="false" value="<?php echo isset($_POST['q3']) ? $email : ''; ?>" required/>
-						</li>
-						<li>
-							<label class="fs-field-label fs-anim-upper" for="q4">Welches Paket sagt Ihnen am meisten zu?</label>
-							<input class="fs-anim-lower" id="q4" name="q4" type="text" placeholder="Onepager / Basic / Premium / Allrounder" spellcheck="false" value="<?php echo isset($_POST['q4']) ? $package : ''; ?>" required/>
-						</li>
-						<li>
-							<label class="fs-field-label fs-anim-upper" for="q5">Gibt es noch etwas, das wir wissen sollten?</label>
-							<textarea class="fs-anim-lower" id="q5" name="q5" placeholder="Was ist Ihr Ziel für Ihre Webseite?" spellcheck="false" autocorrect="off" autocapitalize="off" ><?php echo isset($_POST['q5']) ? $additionalinfo : ''; ?></textarea>
-						</li>
+		<main id="swup">
+			<div class="container">
+				<div class="fs-form-wrap" id="fs-form-wrap">
 
 
-					</ol><!-- /fs-fields -->
-					<button class="fs-submit" type="submit" name="submit">Absenden</button>
-				</form><!-- /fs-form -->
-			</div><!-- /fs-form-wrap -->
-		</div><!-- /container -->
-		<script src="assets/js/classie.js"></script>
-		<script src="assets/js/selectFx.js"></script>
-		<script src="assets/js/fullscreenForm.js"></script>
-		<script>
+					
+					<form id="myform" class="fs-form fs-form-full" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" target="_blank" autocomplete="off">
+						<ol class="fs-fields">
+							<li>
+								<label class="fs-field-label fs-anim-upper" for="q1">Wie lautet Ihr Name?</label>
+								<input class="fs-anim-lower" id="q1" name="q1" type="text" placeholder="Ihr Name" spellcheck="false" value="<?php echo isset($_POST['q1']) ? $name : ''; ?>" required/>
+							</li>
+							<li>
+								<label class="fs-field-label fs-anim-upper" for="q2">Für wen arbeiten Sie?</label>
+								<input class="fs-anim-lower" id="q2" name="q2" type="text" placeholder="Ihre Firma / Selbstständig" spellcheck="false" value="<?php echo isset($_POST['q2']) ? $employer : ''; ?>" required/>
+							</li>
+							<li>
+								<label class="fs-field-label fs-anim-upper" for="q3">Wo dürfen wir Ihnen antworten?</label>
+								<input class="fs-anim-lower" id="q3" name="q3" type="email" placeholder="hello@latify.ch" spellcheck="false" value="<?php echo isset($_POST['q3']) ? $email : ''; ?>" required/>
+							</li>
+							<li>
+								<label class="fs-field-label fs-anim-upper" for="q4">Welches Paket sagt Ihnen am meisten zu?</label>
+								<input class="fs-anim-lower" id="q4" name="q4" type="text" placeholder="Onepager / Basic / Premium / Allrounder" spellcheck="false" value="<?php echo isset($_POST['q4']) ? $package : ''; ?>" required/>
+							</li>
+							<li>
+								<label class="fs-field-label fs-anim-upper" for="q5">Gibt es noch etwas, das wir wissen sollten?</label>
+								<textarea class="fs-anim-lower" id="q5" name="q5" placeholder="Was ist Ihr Ziel für Ihre Webseite?" spellcheck="false" autocorrect="off" autocapitalize="off" ><?php echo isset($_POST['q5']) ? $additionalinfo : ''; ?></textarea>
+							</li>
+
+
+						</ol><!-- /fs-fields -->
+						<button class="fs-submit" type="submit" name="submit">Absenden</button>
+					</form><!-- /fs-form -->
+				</div><!-- /fs-form-wrap -->
+			</div><!-- /container -->
+		</main>
+		<script  data-swup-reload-script="" src="assets/js/classie.js"></script>
+		<script  data-swup-reload-script="" src="assets/js/selectFx.js"></script>
+		<script  data-swup-reload-script="" src="assets/js/fullscreenForm.js"></script>
+		<script data-swup-reload-script="" >
 			(function() {
 				var formWrap = document.getElementById( 'fs-form-wrap' );
 
