4d109c10cfd7b47d7c8ab7a129c9bccf5703b04b
Fabian Amherd <fabian.amherd@gmail.com>
2020-07-02 19:47:24 +0200

    Another try for IE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
 /* Main javascript file */
-
-var ua = window.navigator.userAgent;
-var isIE = /MSIE|Trident/.test(ua);
-
-if ( isIE ) {
-  alert('Still using Internet Explorer? This website does not support this browser. Please view this website on another browser (Google Chrome recommended).')
-}
+if (window.document.documentMode) {
+   alert('Still using Internet Explorer? This website does not support this browser. Please view this website on another browser (Google Chrome recommended).')
+ }
 
 /* Navigation javascript */
 var hamburger = document.querySelector('.hamburger');
